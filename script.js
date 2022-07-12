ed48784eb514c3f67f24f3e9a3ae6bb14ddd27ce
viktor-drok <vityadrok@gmail.com>
2022-07-12 14:27:11 +0300

    add name and age

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict"
+
+document.getElementsByClassName("name-btn")[0].onclick =
+    function() { greeting()
+};
+
+function greeting() {
+    let user;
+    user = document.getElementById("name").value;
+    document.getElementById("user-name").innerHTML = "Привіт, " + user;
+};
+
+document.querySelector(".age-btn").onclick =
+    () => {
+        age();
+    };
+
+function age() {
+    const currentYear = 2022;
+    const birthYear = document.querySelector('.age').value;
+    document.querySelector('.user-age').innerHTML = `Вік - ${currentYear - birthYear} роки`;
+}
